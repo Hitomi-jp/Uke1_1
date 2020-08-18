1af7d8e0a26e825158dc888c792159c97da4e15b
hitom <hitomi.hoshino@gmail.com>
2020-08-18 09:54:01 +0200

    Oppgave 1.1.2.2

--- src/Oppgave1_1_2_2.java
+++ src/Oppgave1_1_2_2.java
@@ -1,7 +1,37 @@
 public class Oppgave1_1_2_2 {
 
-    public static void min(int[ ] a){
+ public static void main(String [] args){
 
+     int[] a = {8, 4, 17, 10, 6, 20, 1, 11, 15, 3, 18, 9, 2, 7, 19};
+
+     int min_index = min(a);
+
+     System.out.println(min_index);
+     System.out.println(a[min_index]);
+
+    }
+
+    static int min(int[] a){
+     //Initialisere med første element
+     int min_value = a[0];
+     int min_index = 0;
+
+     //Loop over alle resterende elementer
+     //et mindre tall.
+     for(int i=1; i<a.length; i++){
+         int value = a[i];
+         int index = i;
+
+         //Sjekk om verdien er mindre enn det vi har sett tidligere.
+         if(value < min_value){
+             min_value = value;
+             min_index = index;
+         }
+
+     }
+
+     //Returnere index til minste verdi.
+     return min_index;
     }
 
 }
